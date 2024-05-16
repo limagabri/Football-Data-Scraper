0be7c3aba9ebb0becd94ebe378d484a5646e3600
Gabriel Lima <gabriel.souza.lima10@hotmail.com>
2024-05-15 23:25:44 -0300

    v1.2
    Adicionado comentários PT-BR e EN-US.
    Adicionado Requirements.txt  e feito o README.MD
    /
    Comments added PT-BR and EN-US.
    Added Requisitos.txt and made README.MD

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# FootballScrapeML
- FootballScrapeML is an automated platform for collecting, analyzing, and predicting football match outcomes. It utilizes scraping for data collection, data analysis with pandas and scikit-learn, and machine learning for accurate predictions.
+# Football Data Scraper
+
+**Language Order / Ordem dos Idiomas:**
+- **English:** Instructions in English are provided first, after Portuguese.
+- **Português:** As instruções em inglês são fornecidas primeiro, depois em português.
+
+This is a Python script to scrape historical football data from the football-data.co.uk website. The script utilizes the requests library to fetch data from the website.
+
+Este é um script Python para realizar scraping de dados históricos de futebol do site football-data.co.uk. O script utiliza a biblioteca requests para obter os dados do site.
+
+## Selected leagues / Ligas selecionadas (93-94)
+
+From 93-94 until 22/23 / De 93-94 até 22/23
+
+- **England Football (Premier League & Divs 1,2,3 & Conference)**
+- **Scotland Football (Premiership & Divs 1,2 & 3)**
+- **Germany Football (Bundesliga 1 & 2)**
+- **Italy Football (Serie A & B)**
+- **Spain Football (La Liga 1 & 2)**
+- **France Football (Ligue 1 & 2)**
+- **Netherlands Football (Erendivisie)**
+- **Belgium Football (Jupiler League)**
+- **Portugal Football (Liga Portugal)**
+- **Turkey Football (Super Lig)**
+- **Greece Football (Premier League)**
+
+
+## How to Use / Como usar
+
+1. **Prerequisites / Pré-requisitos:**
+   - Python 3.12 installed on your system / Python 3.12 instalado no seu sistema.
+   
+2. **Running the Script / Execução do script:**
+   - Run the `football_data_scraper.py` script / Execute o script `football_data_scraper.py`.
+   - Football data will be scraped from the website and saved to CSV files / Os dados de futebol serão raspados do site e salvos em arquivos CSV.
+
+3. **Results / Resultados:**
+   - CSV files will be generated in the specified directory, containing historical football data / Arquivos CSV serão gerados no diretório especificado, contendo dados históricos de futebol.
+
+## Note / Nota
+
+This script is intended for educational purposes and scraping publicly available data. Please ensure compliance with the website's terms of service.
+
+Este script é destinado a fins educacionais e raspagem de dados publicamente disponíveis. Certifique-se de estar em conformidade com os termos de serviço do site.
--- main.py
+++ main.py
@@ -5,20 +5,31 @@ def download_data(league_name, years, league_codes):
     base_url = 'https://www.football-data.co.uk/mmz4281/'
     base_path = 'C:/Users/Gabriel Lima/Documents/github/FootballScrapeML/Historical'
 
+    # Check if the base directory exists, if not, create it
+    # Verifica se o diretório base existe, se não existir, cria-o
     if not os.path.exists(base_path):
         os.makedirs(base_path)
 
+    # Iterate over the years and league codes to download data
+    # Itera sobre os anos e códigos das ligas para baixar os dados
     for year in years:
         for league_code, league_label in league_codes.items():
             file_name = f'{league_label} - {year}.csv'
             file_path = os.path.join(base_path, file_name)
 
+            # Construct the URL for the data file
+            # Constrói a URL para o arquivo de dados
             url = f"{base_url}{year[:2]}{year[3:]}/{league_code}.csv"
             response = requests.head(url)
+
+            # Check if the file exists on the server
+            # Verifica se o arquivo existe no servidor
             if response.status_code == 200:
                 try:
+                    # Download the file and save it
+                    # Baixa o arquivo e o salva
                     response = requests.get(url)
-                    response.raise_for_status()  # Verifica se houve erro na solicitação HTTP
+                    response.raise_for_status()  # Check for HTTP request errors
                     with open(file_path, 'wb') as f:
                         f.write(response.content)
                     print(f'File {file_name} downloaded successfully.')
@@ -28,11 +39,15 @@ def download_data(league_name, years, league_codes):
                 print(f'The file {file_name} does not exist on the server. It was not downloaded.')
 
 def search():
+    # Define the years to search for data
+    # Define os anos para procurar os dados
     years = ['93-94', '94-95', '95-96', '96-97', '97-98', '98-99', '99-00',
              '00-01', '01-02', '02-03', '03-04', '04-05', '05-06', '06-07',
              '07-08', '08-09', '09-10', '10-11', '11-12', '12-13', '13-14',
              '14-15', '15-16', '16-17', '17-18', '18-19', '19-20', '20-21', '21-22', '22-23']
 
+    # Define the league codes and their respective labels
+    # Define os códigos das ligas e seus respectivos rótulos
     league_codes = {
         "E0": "Premier League",
         "E1": "Championship",
@@ -58,6 +73,8 @@ def search():
         "G1": "Super League (Greece)"
     }
 
+    # Call the download_data function with the defined parameters
+    # Chama a função download_data com os parâmetros definidos
     download_data("Football", years, league_codes)
 
 if __name__ == "__main__":
